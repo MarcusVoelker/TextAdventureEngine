6a20605cbea83d137ca643b471a5fd100d608b64
Marcus Völker <marcus@upvoid.com>
2019-11-04 21:48:21 +0100

    Moved parsing into separate function

--- src/MainLoop.hs
+++ src/MainLoop.hs
@@ -15,26 +15,28 @@ import Engine
 import Text.LParse.Parser
 
 import Control.Arrow
+import Control.DoubleContinuations
 import Control.Monad
 import Control.Monad.Trans.State
 import Data.Maybe
 import qualified Data.Map.Strict as M
 import System.IO (hFlush, stdout)
 
+fullParser :: String -> String -> String -> DCont r String (IO GameState)
+fullParser r e i = do
+    rMap <- fst <$> pFunc (tokenizer >>> blocker >>> rooms) r
+    iMap <- fst <$> pFunc (tokenizer >>> blocker >>> items) i
+    es <- fst <$> pFunc (tokenizer >>> blocker >>> Parser.EntityParser.entities rMap iMap) e
+    let initial = initialState (fromJust $ M.lookup "init" rMap) []
+    return $ execStateT (mapM_ (uncurry instantiateEntity) (mapMaybe (\(r,e) -> (,e) <$> r) es)) initial
+
+
 runGame :: IO ()
 runGame = withEngine soundEngine $ do
     roomCode <- readFile "app/rooms.dat"
     entityCode <- readFile "app/entities.dat"
     itemCode <- readFile "app/items.dat"
-    parse (tokenizer >>> blocker >>> rooms) roomCode (\rMap ->
-        parse (tokenizer >>> blocker >>> items) itemCode (\iMap ->
-                parse (tokenizer >>> blocker >>> Parser.EntityParser.entities rMap iMap) entityCode (\es -> do
-                    let initial = initialState (fromJust $ M.lookup "init" rMap)  []
-                    let initial' = execStateT (mapM_ (uncurry instantiateEntity) (mapMaybe (\(r,e) -> (,e) <$> r) es)) initial
-                    initial' >>= mainLoop
-                ) (\e -> putStrLn $ "While parsing entities: " ++ e)
-            ) (\e -> putStrLn $ "While parsing items: " ++ e)
-        ) (\e -> putStrLn $ "While parsing rooms: " ++ e)
+    run (fullParser roomCode entityCode itemCode) (>>= mainLoop) putStrLn
 
 mainLoop :: GameState -> IO ()
 mainLoop s = do
