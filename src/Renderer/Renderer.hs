c2943e118338718a410e79c4cf5587b268d082f3
Marcus Völker <marcus@upvoid.com>
2019-11-18 21:59:13 +0100

    Added abstraction layer in preparation of de-hardcoding renderer

--- src/Renderer/Renderer.hs
+++ src/Renderer/Renderer.hs
@@ -6,45 +6,65 @@ import Logic.Response
 import Engine
 
 import Control.Exception
-import Control.Lens
+import Control.Lens hiding (view)
 import Control.Monad
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
+import qualified Data.Map.Strict as M
 import System.Console.ANSI
 import System.IO (hFlush, stdout)
 
+type WHandle = Int
+
+type View = GameState -> RendererState -> [String]
+
+data Window = Window {
+    _windowHandle :: WHandle,
+    _windowLeft :: Int,
+    _windowTop :: Int,
+    _windowWidth :: Int,
+    _windowHeight :: Int,
+    _windowView :: View
+}
+
 data RendererState = RendererState {
-    _rendererStateTextHistory :: [String]
+    _rendererStateTextHistory :: [String],
+    _rendererStateWindows :: M.Map WHandle Window
 }
 
 type Rendering a = StateT RendererState IO a
 
 makeFields ''RendererState
 
-data Window = Window {
-    _windowLeft :: Int,
-    _windowTop :: Int,
-    _windowWidth :: Int,
-    _windowHeight :: Int
-}
-
 makeFields ''Window
 
-renderWindow :: Window -> IO ()
-renderWindow win = do
+data RenderingException = RenderingException deriving (Show)
+
+instance Exception RenderingException
+
+
+renderWindow :: GameState -> Window -> Rendering ()
+renderWindow gs win = do
     let x = win^.left
     let y = win^.top
     let w = win^.width
     let h = win^.height
+    let v = win^.view
     let bLine = "+" ++('-'<$[3..w])++ "+"
     let iLine = "|" ++(' '<$[3..w])++ "|"
-    setCursorPosition y x
-    putStr bLine
-    forM_ [1..h-2] $ \o -> do
-        setCursorPosition (y+o) x
-        putStr iLine
-    setCursorPosition (y+h-1) x
-    putStr bLine
+    rs <- get
+    lift $ do
+        setCursorPosition y x
+        putStr bLine
+        forM_ [1..h-2] $ \o -> do
+            setCursorPosition (y+o) x
+            putStr iLine
+        setCursorPosition (y+h-1) x
+        putStr bLine
+        let contents = v gs rs
+        forM_ (dropWhile ((<1).fst) $ zip [(y+h-1-length contents)..] contents) $ \(i,s) -> do
+            setCursorPosition i 2
+            putStr s
 
 executeResponse :: GameState -> Response -> Rendering GameState
 executeResponse gs (TextResponse s) = do
@@ -55,7 +75,7 @@ executeResponses :: Responding GameState -> Rendering GameState
 executeResponses (Responding responses gs) = foldM executeResponse gs responses
 
 fallback :: SomeException -> IO (Maybe (Int,Int))
-fallback = const $ return $ return $ (13,160)
+fallback = const $ return $ return $ (14,174)
 
 safeGetTerminalSize :: IO (Maybe (Int,Int))
 safeGetTerminalSize = catch getTerminalSize fallback
@@ -63,25 +83,28 @@ safeGetTerminalSize = catch getTerminalSize fallback
 render :: GameState -> Rendering ()
 render gs = do
     lift clearScreen
-    size <- lift safeGetTerminalSize
-    th <- use textHistory
+    wins <- use windows
+    forM_ wins $ renderWindow gs
+    size <- lift $ safeGetTerminalSize
     lift $ case size of
-        Just (y,x) -> do
-            renderWindow (Window 0 0 x (y-2)) 
-            forM_ (dropWhile ((<1).fst) $ zip [(y-3-length th)..] th) $ \(i,s) -> do
-                setCursorPosition i 2
-                putStr s
-            renderWindow (Window 0 (y-3) x 3)
-            setCursorPosition (y-2) 2
-        Nothing -> putStrLn "Unsupported Terminal!"
+        Just (y,x) -> setCursorPosition (y-2) 2
+        Nothing -> return ()
     lift $ putStr "> "
     lift $ hFlush stdout
 
-initialRendererState :: RendererState
-initialRendererState = RendererState []
+initialRendererState :: IO RendererState
+initialRendererState = do
+    size <- safeGetTerminalSize
+    case size of
+        Just (y,x) -> return $ RendererState [] $ M.fromList [
+            (1,Window 1 0 0 x (y-2) (\_ rs -> rs^.textHistory)),
+            (0,Window 0 0 (y-3) x 3 (\_ _ -> [""]))]
+        Nothing -> throw RenderingException
 
 runRenderer :: Rendering () -> IO ()
-runRenderer action = void $ execStateT action initialRendererState
+runRenderer action = do 
+    irs <- initialRendererState
+    void $ execStateT action irs
 
 renderEngine = Engine 
     clearScreen
